b0836a37fcb8f605c5c9f4083c43cf0230481dc3
Zheng Li <116487537+ZLF369@users.noreply.github.com>
2024-02-18 15:55:42 +0100

    a

---
+++ src/PongGame.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.*;
+
+public class PongGame {
+    public static void main(String[] args) {
+
+        GameFrame frame = new GameFrame();
+
+    }
+}
